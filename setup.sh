2628d5304cdc1e97ce03871c4de330c383f00c53
kcomain <cheunghowui@gmail.com>
2020-03-04 23:12:02 +0800

    disable nano as editor in setup

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ echo "Downloading latest archive..."
 wget https://github.com/kcomain/bfbVoteCounter/archive/master.zip -O /tmp/b53a5a29dfca42de.zip
 
 echo "Unpacking Files"
-yes|unzip /tmp/b53a5a29dfca42de.zip
+yes | unzip /tmp/b53a5a29dfca42de.zip
 cd bfbVoteCounter-master
 
 echo "Please edit the configuration file, or press Control-C or Command-C to cancel this process. Note that the script needs the configuration to work."
 echo "If you wish to use the fancy config editor, please press Control-C or Command-C now and run python3 configGen.py"
 ping google.com -c 10 > /dev/null
 
-if [ -f /bin/nano ]; then
-	nano config.json
-elif [ -f /usr/bin/nvim ]; then
+#if [ -f /bin/nano ]; then
+#	nano config.json
+if [ -f /usr/bin/nvim ]; then
 	nvim config.json
 elif [ -f /usr/bin/vim ]; then
 	vim config.json
