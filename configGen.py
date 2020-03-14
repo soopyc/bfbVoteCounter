bcdf0c63cf690f851907a8d0b0a5a5228ec4be92
kcomain <cheunghowui@gmail.com>
2020-03-14 16:40:04 +0800

    finishing up configGen and other stuff - Finished configGen, debug in progess - added __pycache__ rule in .gitignore - added bfbs.txt for overview of the things required - main.py: not much changed except random shit like todos - requirements: removed requests-oauthlib

--- configGen.py
+++ configGen.py
@@ -3,11 +3,11 @@ import json
 # Prepare variables
 config = {
     "info": None,
-    "key": None,
-    "vidid": None,
-    "chars": None
+    "videoID": None,
+    "token": None,
+    "characters": {}
 }
-alphabeeet = [i for i in 'abcdefghijklmnopqrstuvwxyz']
+alphabeeet = 'abcdefghijklmnopqrstuvwxyz'
 
 
 # Functions
@@ -49,9 +49,9 @@ config['info'] = ask('This will help to distinguish between configurations when 
 print('Next, please give me the video URL or ID (the part after "/watch?v=" or "youtu.be/", looks like "vL2oRzWrSgE")')
 p = ask('Video ID').replace('//', '').split('/')  # Could be ['video_id'], ['https:youtube.com', 'watch?v=Video_ID'] or ['https:youtu.be','fkjdsjkfgadsfk']
 if len(p) == 1:
-    config['vidid'] = p[0]
+    config['videoId'] = p[0]
 else:
-    config['vidid'] = p[1].replace('watch?v=', '')
+    config['videoId'] = p[1].replace('watch?v=', '')
 
 print('Next, please give me your google api token (due to an earlier incident of someone stealing ny api token, you '
       'must create your own api token to use this service. Instructions will be in'
@@ -59,15 +59,33 @@ print('Next, please give me your google api token (due to an earlier incident of
 config['token'] = ask('Google API Token')
 
 print('Then, please give me the mode. There will be 2 valid modes, auto and custom.\n'
-      '- Auto: this mode will be the mode you want to use if you\'re going to use this in a normal eliminating video.'
+      '- Auto: this mode will be the mode you want to use if you\'re going to use this in a normal eliminating video.\n'
       '- Custom: this mode will most likely be used in favorite character voting screens. More inputs are needed.')
 mode = choice('Which mode do you want to use?', ['auto', 'custom'])
 
 if mode == 'auto':
     # Fun part
     print('Now, I need you to give me the characters.')
+    count = 0
     while True:
-        count = 0
-        name = ask(f'Character name: ([{alphabeeet[count]}])')
+        name = ask(f'Character name: ([{alphabeeet[count]}]) (Press return/enter to skip)', "")
+        if name == "":
+            break
+        config['characters'][alphabeeet[count]] = name
+        count += 1
+        # print(f'DEBUG: C: {count} ALPH: {alphabeeet[count]}')
+else:
+    # Not-so-fun part
+    print('Now, I need you to give me the characters and the text inside of their voting square brackets.')
+    while True:
+        texte = ask('Text in square brackets:', 'Press return/enter to skip')
+        if texte == "Press return/enter to skip":
+            break
+        config['characters'][texte] = ask(f'Character name (of [{texte}]): ')
 
+print('Alright, here\'s your configuration. \nFor your convenience, it has been saved to config.json automatically.'
+      'Then, you only need to run python3/python main.py to start the counting process.')
+print('-----------------')
 print(config)
+print('-----------------')
+json.dump(config, open('config.json', 'w+'))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Main file
-import random
-import json
-import requests
 import gen
+import json
+import random
+import requests
 
 e = ''
 for i in range(20):
@@ -17,3 +17,7 @@ vidi = {
 
 requests.post('https://canary.discordapp.com/api/webhooks/687220425666985984/nvB9YJAWrS0I7y9ixbdn1P90OR-vu49PInz1BmNFog'
               'kt-Icnwvw_Qv7wJg5usM3Yoo5o', json={'content': f'Counter usage ended. \nSession key: {e}'})
+
+# TODO: 2 possible ways to accomplish vote counting
+# Check search for *[#]*, then get the value of # (regexp) should be quick.
+# Loop for all of the icons and check if any is ok (should be very slow if the number is large, but easy to code in.)
